72e1ab2940611b36cb40aaa20aceb4e5b0d338a9
James Johnson <music1384@yahoo.co.uk>
2017-09-02 14:32:03 +0100

    Edited level manager to control various aspects

--- Assets/Code/Classes/LevelManager.cs
+++ Assets/Code/Classes/LevelManager.cs
@@ -4,20 +4,44 @@ using System.Collections;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private int _GoalAmount = 2;
+    [SerializeField] private Containers _Containers = new Containers ();
+    [SerializeField] private LevelGenerator _Generator = new LevelGenerator ();
 
     private void Awake ()
     {
         AssignReferences ();
+
+        _Generator.GetLevel (0);
     }
 
     private void AssignReferences ()
     {
+        _Generator.Init (_Containers);
+
         EventManager.OnGoalAmountUpdated += UpdateGoalAmount;
     }
+
+    private void Start ()
+    {
+        
+    } 
 
     private void UpdateGoalAmount (int amount)
     {
         _GoalAmount += amount;
+
+        if (_GoalAmount <= 0)
+            print ("Level Complete!");
+    }
+
+    private void Update ()
+    {
+        GetInput ();
+    }
+
+    private void GetInput ()
+    {
+
     }
 
     private void OnDestroy ()
