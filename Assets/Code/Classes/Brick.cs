02b2f6d8be60f198d4b4890fd294b09fd6f9166d
James Johnson <music1384@yahoo.co.uk>
2017-09-02 11:25:35 +0100

    Added basic goal check functionality.

--- Assets/Code/Classes/Brick.cs
+++ Assets/Code/Classes/Brick.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Brick : MonoBehaviour
 {
+    [SerializeField] private Color _DefaultColor = Color.red;
+    [SerializeField] private Color _GoalColor = Color.magenta;
+
+    private Renderer _Renderer = null;
     private Transform _Transform = null;
 
     private void Awake ()
@@ -14,7 +18,13 @@ public class Brick : MonoBehaviour
 
     private void AssignReferences ()
     {
+        _Renderer = GetComponent<Renderer> ();
         _Transform = GetComponent<Transform> ();
+    }
+
+    private void Start ()
+    {
+        _Renderer.material.color = _DefaultColor;
     }
 
     public bool CanBePushed (Vector3 dir)
@@ -22,6 +32,7 @@ public class Brick : MonoBehaviour
         if (CanMove (dir))
         {
             _Transform.position += dir;
+            CheckForGoal ();
             return true;
         }
 
@@ -30,13 +41,41 @@ public class Brick : MonoBehaviour
 
     private bool CanMove (Vector3 dir)
     {
-        var end = _Transform.position + dir;
         var info = new RaycastHit ();
 
-        if (Physics.Linecast (_Transform.position, end, out info))
-            if (info.collider.CompareTag ("Wall") || info.collider.CompareTag("Brick"))
+        if (Physics.Linecast (_Transform.position, _Transform.position + dir, out info))
+        {
+            if (info.collider.CompareTag ("Wall") || info.collider.CompareTag ("Brick"))
                 return false;
+        }
 
         return true;
+    }
+
+    private void CheckForGoal ()
+    {
+        var info = new RaycastHit ();
+
+        if (Physics.Linecast (_Transform.position, _Transform.position + Vector3.forward, out info))
+        {
+            if (info.collider.CompareTag ("Goal"))
+                InGoal (true);
+            else
+                InGoal (false);
+        }
+    }
+
+    private void InGoal (bool isInGoal)
+    {
+        //TODO: Inform level manager brick is in place.
+
+        if(isInGoal)
+        {
+            _Renderer.material.color = _GoalColor;
+        }
+        else
+        {
+            _Renderer.material.color = _DefaultColor;
+        }
     }
 }
