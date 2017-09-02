5d02a23f779e6fa242fb46107a73c174cc9b7fe5
James Johnson <music1384@yahoo.co.uk>
2017-09-03 00:48:39 +0100

    Implemented UI controller for all game UI stataes

---
+++ Assets/Code/Classes/User Interface/UIController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _Screens = null;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+
+        EventManager.OnStateChanged += StateChanged;
+        EventManager.Changestate (GameStates.InGame);
+    }
+
+    private void AssignReferences ()
+    {
+        _Screens = new GameObject[transform.childCount];
+
+        for (int i = 0; i < _Screens.Length; i++)
+            _Screens[i] = transform.GetChild (i).gameObject;
+    }
+
+    private void StateChanged (GameStates state)
+    {
+        switch (state)
+        {
+            case GameStates.Menu:
+                SwitchScreen (0);
+                break;
+            case GameStates.Options:
+                SwitchScreen (1);
+                break;
+            case GameStates.Credits:
+                SwitchScreen (2);
+                break;
+            case GameStates.LevelSelect:
+                SwitchScreen (3);
+                break;
+            case GameStates.InGame:
+                SwitchScreen (4);
+                break;
+            case GameStates.LevelComplete:
+                SwitchScreen (5);
+                break;
+            case GameStates.InGameOptions:
+                SwitchScreen (6);
+                break;
+        }
+    }
+
+    private void SwitchScreen (int screenNumber)
+    {
+        for (int i = 0; i < _Screens.Length; i++)
+        {
+            if (i == screenNumber)
+                _Screens[i].SetActive (true);
+            else
+                _Screens[i].SetActive (false);
+        }
+    }
+
+    private void OnDestroy ()
+    {
+        EventManager.OnStateChanged -= StateChanged;
+    }
+}
