21892a74b3e6f0254415e2fe326db086e0bd4854
James Johnson <music1384@yahoo.co.uk>
2017-09-02 12:10:39 +0100

    Added a levelmanager to control level states and scores

---
+++ Assets/Code/Classes/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private int _GoalAmount = 2;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+    }
+
+    private void AssignReferences ()
+    {
+        EventManager.OnGoalAmountUpdated += UpdateGoalAmount;
+    }
+
+    private void UpdateGoalAmount (int amount)
+    {
+        _GoalAmount += amount;
+    }
+
+    private void OnDestroy ()
+    {
+        EventManager.OnGoalAmountUpdated -= UpdateGoalAmount;
+    }
+}
