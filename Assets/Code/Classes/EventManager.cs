e2cc4207b2ae6ed6bfc5387bcff9a96d33e34f62
James Johnson <music1384@yahoo.co.uk>
2017-09-03 00:48:24 +0100

    Added new event listener for state changes.

--- Assets/Code/Classes/EventManager.cs
+++ Assets/Code/Classes/EventManager.cs
@@ -1,16 +1,23 @@
 ﻿public delegate void GoalAmountUpdated (int amount);
+public delegate void StateChanged (GameStates state);
 public delegate void BrickPushed (int amount, bool isCaller);
 public delegate void PlayerMoved (int amount, bool isCaller);
 
 public static class EventManager
 {
     public static event GoalAmountUpdated OnGoalAmountUpdated;
+    public static event StateChanged OnStateChanged;
     public static event BrickPushed OnBrickPushed;
     public static event PlayerMoved OnPlayerMoved;
 
     public static void UpdateGoalAmount (int amount)
     {
         OnGoalAmountUpdated (amount);
+    }
+
+    public static void Changestate (GameStates state)
+    {
+        OnStateChanged (state);
     }
 
     public static void PushBrick (int amount, bool isCaller)
