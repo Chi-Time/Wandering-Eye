5c42c184f7b212dc2916e0122a533d13ce4e6c84
James Johnson <music1384@yahoo.co.uk>
2017-09-02 12:10:21 +0100

    Added an eventmanager for updating scores etc.

---
+++ Assets/Code/Classes/EventManager.cs
@@ -0,0 +1,11 @@
+﻿public delegate void GoalAmountUpdated (int amount);
+
+public static class EventManager
+{
+    public static event GoalAmountUpdated OnGoalAmountUpdated;
+
+    public static void UpdateGoalAmount (int amount)
+    {
+        OnGoalAmountUpdated (amount);
+    }
+}
