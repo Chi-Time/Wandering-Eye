fc8e72cf4072286ccdb43b6225eeece8b0962745
James Johnson <music1384@yahoo.co.uk>
2017-09-03 01:37:58 +0100

    Create basic GameController for switiching levels

---
+++ Assets/Code/Classes/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private int _CurrentLevel = 0;
+
+    private LevelManager _LevelManager = null;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+    }
+
+    private void AssignReferences ()
+    {
+        _LevelManager = GetComponent<LevelManager> ();
+    }
+
+    public void NextLevel ()
+    {
+        _LevelManager._Generator.GetLevel (_CurrentLevel += 1);
+    }
+
+    public void LastLevel ()
+    {
+        _LevelManager._Generator.GetLevel (_CurrentLevel -= 1);
+    }
+}
